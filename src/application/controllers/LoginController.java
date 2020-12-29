ef8ce86b7d79c15292c8c1f14ab924321f4ff955
Melvin NAMONT <melvin.namont@gmail.com>
2020-12-29 12:58:58 +0100

    remove spacess

--- src/application/controllers/LoginController.java
+++ src/application/controllers/LoginController.java
@@ -8,13 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import serverConection.ConnectionManager;
-
-
 import com.jfoenix.controls.JFXButton;
-
 import application.models.LoginModel;
-
-
 
 public class LoginController{
 
